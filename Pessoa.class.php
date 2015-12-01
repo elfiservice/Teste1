79cdfa936a9eb14b7fb9abc4812cf887543ae314
elfiservice <elfiservice@hotmail.com>
2015-12-01 08:57:04 -0300

    Adicionada classe Pessoa ao projeto

---
+++ Pessoa.class.php
@@ -0,0 +1,33 @@
+<?php
+
+class Pessoa{
+
+	private  $primeiroNome;
+	private  $segundoNome;
+
+	public function __construct($primeironome, $segundonome){
+
+		$this->primeiroNome = $primeironome;
+		$this->segundoNome = $segundonome;
+
+
+	}
+
+	public function getName(){
+		return $this->primeiroNome." ".$this->segundoNome;
+	}
+
+	public function setPrimeiroNome($primeironomeset){
+		$this->primeiroNome = $primeironomeset;
+	}
+
+	public function setSegundoNome($segundonomeset){
+		$this->segundoNome = $segundonomeset;
+	}
+
+	//	public abstract function esporte();
+
+}
+
+
+?>
